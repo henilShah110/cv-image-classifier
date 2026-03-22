358928be53173f2551478b55f3607ffc534b6ac0
henilshah110 <henilshah110@gmail.com>
2026-03-22 15:03:41 +0530

    Changed predict.py to use ResNet18

--- predict.py
+++ predict.py
@@ -1,18 +1,21 @@
 import torch
 from PIL import Image
 from torchvision import transforms
+from torchvision.models import resnet18, ResNet18_Weights
 from model import CNN
 
 classes = ['buildings', 'forest', 'glacier', 'mountain', 'sea', 'street']
 
 # Load model
-model = CNN(num_classes=6)
+#model = CNN(num_classes=6)
+model = resnet18(weights=ResNet18_Weights.DEFAULT)
+model.fc = torch.nn.Linear(model.fc.in_features, 6)
 model.load_state_dict(torch.load("model.pth"))
 model.eval()
 
 # Transform
 transform = transforms.Compose([
-    transforms.Resize((150, 150)),
+    transforms.Resize((128, 128)),
     transforms.ToTensor()
 ])
 
